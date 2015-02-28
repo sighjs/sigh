8cad65cd8f800edca06924f81946740b7c682ec7
James Pike <jpike@chilon.net>
2015-02-28 14:15:47 +0000

    Simplify tests.

--- test/src/glob.spec.js
+++ test/src/glob.spec.js
@@ -9,43 +9,41 @@ var rm = Promise.promisify(fse.remove)
 
 require('chai').should()
 
-var MOCK_PROJECT_DIR = 'test/fixtures/simple-project'
-var TMP_DIR = 'test/tmp'
+var FIXTURE_PATH = 'test/fixtures/simple-project'
+var TMP_PATH = 'test/tmp'
+var FIXTURE_FILES = [
+  FIXTURE_PATH + '/file1.js',
+  FIXTURE_PATH + '/file2.js'
+]
 
 describe('glob plugin', () => {
   it('globs a wildcard', () => {
-    return glob(null, false, MOCK_PROJECT_DIR + '/*.js').toPromise().then(updates => {
+    return glob(null, false, FIXTURE_PATH + '/*.js').toPromise().then(updates => {
       updates.length.should.equal(2)
-      _.pluck(updates, 'path').sort().should.eql([
-        MOCK_PROJECT_DIR + '/file1.js',
-        MOCK_PROJECT_DIR + '/file2.js'
-      ])
+      _.pluck(updates, 'path').sort().should.eql(FIXTURE_FILES)
       updates.forEach(file => { file.type.should.equal('add') })
     })
   })
 
   it('globs two wildcards', () => {
-    return glob(null, false, MOCK_PROJECT_DIR + '/*1.js', MOCK_PROJECT_DIR + '/*2.js')
+    return glob(null, false, FIXTURE_PATH + '/*1.js', FIXTURE_PATH + '/*2.js')
     .toPromise()
     .then(updates => {
       updates.length.should.equal(2)
-      _.pluck(updates, 'path').sort().should.eql([
-        MOCK_PROJECT_DIR + '/file1.js',
-        MOCK_PROJECT_DIR + '/file2.js'
-      ])
+      _.pluck(updates, 'path').sort().should.eql(FIXTURE_FILES)
       updates.forEach(file => { file.type.should.equal('add') })
     })
   })
 
   it('detects change to file matching globbed pattern', () => {
-    return rm(TMP_DIR).then(() => {
-      return copy(MOCK_PROJECT_DIR, TMP_DIR)
+    return rm(TMP_PATH).then(() => {
+      return copy(FIXTURE_PATH, TMP_PATH)
     })
     .then(() => {
       return new Promise(function(resolve) {
         var isWatching = false
-        var changeFile = TMP_DIR + '/file1.js'
-        glob(null, true, TMP_DIR + '/*.js').onValue(updates => {
+        var changeFile = TMP_PATH + '/file1.js'
+        glob(null, true, TMP_PATH + '/*.js').onValue(updates => {
           if (! isWatching) {
             updates.length.should.equal(2)
             isWatching = true
@@ -65,7 +63,7 @@ describe('glob plugin', () => {
     (() => {
       // the first argument is the stream and must be null for the glob operation,
       // this is a special value passed during the stream construction in src/api.js
-      glob('some stream', false, MOCK_PROJECT_DIR + '/*.js')
+      glob('some stream', false, FIXTURE_PATH + '/*.js')
     }).should.throw()
   })
 })
