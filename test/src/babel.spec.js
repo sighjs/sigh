debcb2683354186cd00f2388ce3c927952887607
James Pike <jpike@chilon.net>
2015-03-05 19:00:39 +0000

    Babel test: verify more source map elements.

--- test/src/babel.spec.js
+++ test/src/babel.spec.js
@@ -9,14 +9,13 @@ require('chai').should()
 describe('babel plugin', () => {
   it('compiles a single add event', () => {
     var data = 'var pump = () => "pumper"'
-    var stream = Bacon.once([
-      new Event({
-        basePath: 'root',
-        path: 'root/subdir/file.js',
-        type: 'add',
-        data
-      })
-    ])
+    var event = new Event({
+      basePath: 'root',
+      path: 'root/subdir/file.js',
+      type: 'add',
+      data
+    })
+    var stream = Bacon.once([ event ])
 
     return babel(stream).toPromise().then(events => {
       events.length.should.equal(1)
@@ -24,8 +23,8 @@ describe('babel plugin', () => {
       var { data, sourceMap } = events[0]
       data.should.equal('define("subdir/file", ["exports"], function (exports) {\n  "use strict";\n\n  var pump = function () {\n    return "pumper";\n  };\n});')
       sourceMap.mappings.should.equal(';;;AAAA,MAAI,IAAI,GAAG;WAAM,QAAQ;GAAA,CAAA')
-
-      console.log('TODO: test more elements in', sourceMap)
+      sourceMap.file.should.equal(event.path)
+      sourceMap.sourceRoot.should.equal(event.basePath)
     })
   })
 
