f712406deaaaeb87bb2dce1d752afbbafd8f11d4
James Pike <jpike@chilon.net>
2015-03-07 20:02:59 +0000

    Simplify Event class.

--- src/event.js
+++ src/event.js
@@ -13,6 +13,7 @@ export default class {
     if (fields.opTreeIndex)
       this.opTreeIndex = fields.opTreeIndex
 
+    // setting the data here can also add a source map
     if (this.type !== 'remove')
       this.data = fields.data !== undefined ? fields.data : readFileSync(this.path).toString()
 
@@ -20,42 +21,8 @@ export default class {
       this.basePath = fields.basePath
 
     if (fields.sourceMap) {
-      this.sourceMap = fields.sourceMap
+      this.applySourceMap(fields.sourceMap)
     }
-    else {
-      if (this.supportsSourceMap)
-        this.hasIdentitySourceMap = true // it is lazy initialised
-    }
-  }
-
-  get fileType() {
-    return this.path.substring(this.path.lastIndexOf('.') + 1)
-  }
-
-  get sourceMap() {
-    if (this.hasIdentitySourceMap && ! this._sourceMap)
-      this._sourceMap = generateIdentitySourceMap(this.fileType, this.path, this.data)
-
-    return this._sourceMap
-  }
-
-  set sourceMap(value) {
-    this._sourceMap = value
-  }
-
-  applySourceMap(sourceMap) {
-    if (this.hasIdentitySourceMap) {
-      this.hasIdentitySourceMap = false
-      this.sourceMap = sourceMap
-    }
-    else {
-      this.sourceMap = applySourceMap(this.sourceMap, sourceMap)
-    }
-  }
-
-  get supportsSourceMap() {
-    var { fileType } = this
-    return fileType === 'js' || fileType === 'css'
   }
 
   // does this need to be a property?
@@ -65,5 +32,44 @@ export default class {
 
     return this.path.indexOf(this.basePath) === 0 ?
       this.path.substr(this.basePath.length + 1) : this.path
+  }
+
+  set data(value) {
+    this._data = value
+    // TODO: strip/parse source map comment
+  }
+  get data() { return this._data }
+
+  /**
+   * @return {Number} The number of lines in the data
+   */
+  get lineCount() {
+    // TODO: be more efficient
+    return this.data.split('\n').length
+  }
+
+  get fileType() {
+    return this.path.substring(this.path.lastIndexOf('.') + 1)
+  }
+
+  get sourceMap() {
+    if (! this._sourceMap)
+      this._sourceMap = generateIdentitySourceMap(this.fileType, this.path, this.data)
+
+    return this._sourceMap
+  }
+
+  applySourceMap(sourceMap) {
+    if (! this.sourceMap) {
+      this._sourceMap = sourceMap
+    }
+    else {
+      this._sourceMap = applySourceMap(this.sourceMap, sourceMap)
+    }
+  }
+
+  get supportsSourceMap() {
+    var { fileType } = this
+    return fileType === 'js' || fileType === 'css'
   }
 }
