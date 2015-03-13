78119ae070f167a6e7864ee83527b0daec533b35
James Pike <jpike@chilon.net>
2015-03-13 12:38:55 +0000

    PipelineCompiler test uses plugin helper.

--- test/src/PipelineCompiler.spec.js
+++ test/src/PipelineCompiler.spec.js
@@ -2,28 +2,30 @@ import Bacon from 'baconjs'
 import Promise from 'bluebird'
 import PipelineCompiler from '../lib/PipelineCompiler'
 
+import { plugin } from './helper'
+
 var should = require('chai').should()
 
 describe('PipelineCompiler', () => {
   it('should create appropriate stream from array', () => {
     var compiler = new PipelineCompiler
     return compiler.compile([
-      { plugin(op) {
+      plugin(op => {
         should.not.exist(op.stream)
         return Bacon.once(1)
-      } },
-      { plugin(op) { return op.stream.map(v => v + 1) } }
+      }),
+      plugin(op => { return op.stream.map(v => v + 1) })
     ])
     .then(stream => stream.toPromise(Promise).then(value => value.should.equal(2)))
   })
 
   it('should create stream from stream, passing watch option', () => {
     var compiler = new PipelineCompiler({ watch: true })
-    return compiler.compile({ plugin(op) {
+    return compiler.compile(plugin(op => {
       op.watch.should.be.true
       should.not.exist(op.stream)
       return Bacon.once(420)
-    } })
+    }))
     .then(
       stream => stream.toPromise(Promise).then(value => value.should.equal(420))
     )
@@ -31,23 +33,20 @@ describe('PipelineCompiler', () => {
 
   it('should pass arguments to plugin', () => {
     var compiler = new PipelineCompiler
-    return compiler.compile({
-      plugin(op, arg1, arg2) {
+    return compiler.compile(plugin((op, arg1, arg2) => {
         should.not.exist(op.watch)
         should.not.exist(op.stream)
         return Bacon.once(arg1 + arg2)
-      },
-      args: [ 7, 11 ]
-    })
+    }, 7, 11))
     .then(stream => stream.toPromise(Promise).then(value => value.should.equal(18)))
   })
 
   it('should pass treeIndex and observe nextTreeIndex', () => {
     var compiler = new PipelineCompiler
     return compiler.compile([
-      { plugin(op) { op.treeIndex.should.equal(1) } },
-      { plugin(op) { op.treeIndex.should.equal(2), op.treeIndex = 4 } },
-      { plugin(op) { op.treeIndex.should.equal(4) } }
+      plugin(op => { op.treeIndex.should.equal(1) }),
+      plugin(op => { op.treeIndex.should.equal(2), op.treeIndex = 4 }),
+      plugin(op => { op.treeIndex.should.equal(4) })
     ])
   })
 })
