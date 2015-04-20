164eefe492676601a0093835fe97503deda93f3d
James Pike <jpike@chilon.net>
2015-04-20 22:12:18 +0100

    Remove promise.js

--- src/promise.js
+++
@@ -1,6 +0,0 @@
-// argument forwarding promise delay
-export function delay(ms, arg) {
-  var deferred = Promise.pending()
-  setTimeout(() => deferred.fulfill(arg), ms)
-  return deferred.promise;
-}
