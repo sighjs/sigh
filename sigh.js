7a2895161463bbac95474e41e57e794eb617c23a
James Pike <jpike@chilon.net>
2015-05-08 19:14:39 +0100

    pipeline-names-like-this

--- sigh.js
+++ sigh.js
@@ -1,22 +1,22 @@
 var glob, pipeline, babel, debounce, write, mocha
 
 module.exports = function(pipelines) {
-  pipelines.buildSource = [
+  pipelines['build-sources'] = [
     glob({ basePath: 'src' }, '*.js', 'plugin/*.js'),
     babel({ modules: 'common' }),
     write('lib')
   ]
 
-  pipelines.buildTests = [
+  pipelines['build-tests'] = [
     glob({ basePath: 'src/test' }, '*.js', 'plugin/*.js'),
     babel({ modules: 'common' }),
     write('lib/test')
   ]
 
-  pipelines.alias.build = ['test:js', 'source:js']
+  pipelines.alias.build = ['build-sources', 'build-tests']
 
-  pipelines.runTests = [
-    pipeline('source:js', 'test:js'),
+  pipelines['run-tests'] = [
+    pipeline('build-sources', 'build-tests'),
     debounce(700),
     pipeline({ activate: true }, 'mocha')
   ]
