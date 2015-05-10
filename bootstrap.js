6a8cb7c68c10a6ff9eafb5bec684aa9e70aef582
James Pike <jpike@chilon.net>
2015-05-10 13:28:05 +0100

    Get rid of grunt dev dependency.

--- bootstrap.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-
-require('source-map-support').install()
-
-var invoke = require('./lib/api').invoke
-invoke({ watch: true, jobs: 4 })
---
+++ bootstrap.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+./node_modules/.bin/babel --modules common --source-maps --out-dir lib src
