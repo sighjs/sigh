2634f3f5b381719f29c77fa018061c5f50001a65
James Pike <jpike@chilon.net>
2016-04-16 17:41:21 +0100

    first attempt at collectInitials option

--- src/plugin/merge.js
+++ src/plugin/merge.js
@@ -4,12 +4,16 @@ import _ from 'lodash'
 
 import { bufferingDebounce } from 'sigh-core/lib/stream'
 
-var DEFAULT_DEBOUNCE = 200
-
 export default function(op, ...pipelines) {
+  let collectInitials = false
+  if (! pipelines.stream) {
+    const opts = pipelines.unshift()
+    collectInitials = opts.collectInitials
+  }
+
   // Promise.map(..., { concurrency: 1 }) delivers the items to the iterator
   // out of order which messes with opTreeIndex ordering.
-  return Promise.reduce(
+  const stream = Promise.reduce(
     pipelines,
     (streams, pipeline) => {
       return op.compiler.compile(pipeline, op.stream || null)
@@ -21,4 +25,17 @@ export default function(op, ...pipelines) {
     []
   )
   .then(streams => Bacon.mergeAll(streams.filter(stream => stream !== op.compiler.initStream)))
+
+  if (collectInitials) {
+    let { length: nStreamEventsLeft } = pipelines
+
+    return stream.flatMapLatest(events => {
+      if (events.every(event => event.initPhase)) {
+        return --nStreamEventsLeft ? Bacon.never() :
+                                     Bacon.mergeAll([ Bacon.constant(events), stream ])
+      }
+    })
+  }
+
+  return stream
 }
